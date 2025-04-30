dd7567d3afdd339e8d392d240c0db2434ee2f24d
captEze <ezetheophilus05@gmail.com>
2025-04-30 10:55:52 +0100

    Updated apis to new api gateway

--- src/pages/StoreSetup.jsx
+++ src/pages/StoreSetup.jsx
@@ -26,7 +26,7 @@ const SetupStore = () => {
     const [step, setStep] = useState(1);
 
     useEffect(() => {
-        axios.get("https://ps.marketprime.io/v1/banks").then((res) => {
+        axios.get("https://default.marketprime.io/ps/v1/banks").then((res) => {
             setBanks(res.data.data);
         });
     }, []);
